196ea34f24337808c54a283f227194d05910d3ac
Christian Valesares <cvalesares@gmail.com>
2021-11-02 13:47:59 -0400

    add refactor to model tests and movie update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,17 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.actors.new(actor_params)
+    
+    redirect_to "/movies/#{@movie.id}"
+  end
+
+  private
+    def actor_params
+      result = {name: params[:name],
+        age: params[:age]}
+    end
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -51,16 +51,3 @@ RSpec.describe 'Movies Show' do
     expect(page).to have_content("Scarlett Johansson")
   end
 end
-
-# Story 3
-# Add an Actor to a Movie
-#
-# As a user,
-# When I visit a movie show page,
-# I do not see any actors listed that are not part of the movie
-# And I see a form to add an actor to this movie
-# When I fill in the form with the name of an actor that exists in the database
-# And I click submit
-# Then I am redirected back to that movie's show page
-# And I see the actor's name is now listed
-# (You do not have to test for a sad path, for example if the name submitted is not an existing actor)
